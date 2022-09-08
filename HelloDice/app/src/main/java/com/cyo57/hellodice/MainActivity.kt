b91ea79a46d45df1e112e0e4a3b653182254e6e5
cyo57 <cyo57@outlook.com>
2022-09-08 15:57:29 +0800

    Add ImageView
    改成图片应用

--- HelloDice/app/src/main/java/com/cyo57/hellodice/MainActivity.kt
+++ HelloDice/app/src/main/java/com/cyo57/hellodice/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.cyo57.hellodice
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,25 +18,33 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.buttonRoll)
         rollButton.setOnClickListener { rollDice() }
-
-
+        // 应用首次启动显示骰子图片
+        rollDice()
     }
+
+
     fun rollDice() {
-        val textDice1: TextView = findViewById(R.id.textDice1)
-        val textDice2: TextView = findViewById(R.id.textDice2)
+        //val textDice1: TextView = findViewById(R.id.textDice1)
+        val imageDice: ImageView = findViewById(R.id.imageView)
         val myDice1 = Dice(6)
-        val myDice2 = Dice(6)
-        textDice1.text = myDice1.roll().toString()
-        textDice2.text = myDice2.roll().toString()
-        Toast.makeText(this, "Roll completed", Toast.LENGTH_SHORT).show()
+        val resultDice = myDice1.roll()
+        //Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT).show()
+
+        val imageRes = when (resultDice) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imageDice.setImageResource(imageRes)
     }
 }
-
 
 
 class Dice(val sides: Int) {
     fun roll(): Int {
         return (1..sides).random()
     }
-
 }
